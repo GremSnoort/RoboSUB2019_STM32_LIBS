ba376e7069d8f1a67576b4ca4763f6b484938c27
unknown <grem_snoort@protonmail.com>
2019-01-14 20:13:46 +0900

    si7021 added

--- Si7021_temperature_humidity/Inc/Si7021_drv.h
+++ Si7021_temperature_humidity/Inc/Si7021_drv.h
@@ -16,8 +16,8 @@
 
 static const uint16_t Si7021_ADDR = (0x40<<1); // Si7021 I2C address
 
-#define Si7021_RES0 			0
-#define Si7021_RES1 			7
+#define Si7021_RES0				0
+#define Si7021_RES1				7
 #define Si7021_VDDS				6
 #define Si7021_HTRE				2
 
@@ -28,16 +28,16 @@ static const uint16_t Si7021_ADDR = (0x40<<1); // Si7021 I2C address
 #define Si7021_TEMP_NHM   			0xF3 // Measure Temperature, No Hold Master Mode
 #define Si7021_TEMP_AH    			0xE0 // Read Temperature Value from Previous RH Measurement
 #define Si7021_RESET      			0xFE // Reset
-#define Si7021_W_RHT_U_REG      0xE6 // Write RH/T User Register 1
-#define Si7021_R_RHT_U_REG      0xE7 // Read RH/T User Register 1
-#define Si7021_W_HEATER_C_REG   0x51 // Write Heater Control Register
-#define Si7021_R_HEATER_C_REG   0x11 // Read Heater Control Register
-#define Si7021_R_ID_BYTE11      0xFA // Read Electronic ID 1st Byte, first part
-#define Si7021_R_ID_BYTE12      0x0F // Read Electronic ID 1st Byte, second part
-#define Si7021_R_ID_BYTE21      0xFC // Read Electronic ID 2nd Byte, first part
-#define Si7021_R_ID_BYTE22      0xC9 // Read Electronic ID 2nd Byte, second part
-#define Si7021_R_FIRM_REV1      0x84 // Read Firmware Revision, first part
-#define Si7021_R_FIRM_REV2      0xB8  // Read Firmware Revision, second part
+#define Si7021_W_RHT_U_REG      	0xE6 // Write RH/T User Register 1
+#define Si7021_R_RHT_U_REG      	0xE7 // Read RH/T User Register 1
+#define Si7021_W_HEATER_C_REG   	0x51 // Write Heater Control Register
+#define Si7021_R_HEATER_C_REG   	0x11 // Read Heater Control Register
+#define Si7021_R_ID_BYTE11      	0xFA // Read Electronic ID 1st Byte, first part
+#define Si7021_R_ID_BYTE12      	0x0F // Read Electronic ID 1st Byte, second part
+#define Si7021_R_ID_BYTE21      	0xFC // Read Electronic ID 2nd Byte, first part
+#define Si7021_R_ID_BYTE22      	0xC9 // Read Electronic ID 2nd Byte, second part
+#define Si7021_R_FIRM_REV1      	0x84 // Read Firmware Revision, first part
+#define Si7021_R_FIRM_REV2      	0xB8  // Read Firmware Revision, second part
 
 typedef struct Si7021Device
 {	
